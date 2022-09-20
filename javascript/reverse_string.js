5274d8420fb75286995552e152c336e7cd479c2c
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-09-20 15:51:12 -0400

    solved

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,12 @@
 function reverseString(str) {
-  // type your code here
+  let wrd = "";
+
+  for (let index = str.length - 1; index > -1; --index) {
+    wrd = wrd + str[index];
+    console.log(wrd);
+    console.log(index);
+  }
+  return wrd;
 }
 
 if (require.main === module) {
@@ -16,4 +23,11 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+// get length of string - 1
+// slice last letter of string to new array
+// repeat until length is 0
+// join new array into a string
+// return sring
+
 // And a written explanation of your solution
